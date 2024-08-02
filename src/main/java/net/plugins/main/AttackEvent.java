938cc52e37cdc9ff18bf6bde40a2953973b0f052
Redgina1d <firek375@gmail.com>
2024-08-01 23:41:30 -0300

    Removed unused dependencies and imports

--- src/main/java/net/plugins/main/AttackEvent.java
+++ src/main/java/net/plugins/main/AttackEvent.java
@@ -4,9 +4,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
@@ -18,7 +15,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
